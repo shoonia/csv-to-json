647f58d1cf0f5d728a8a3d3edbf634618c245889
shoonia <zaytsev126@gmail.com>
2018-10-26 13:16:00 +0300

    No back to CSV

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const csvInput = path.resolve(process.argv[2] || 'solution.csv');
 
 const distDir = path.resolve(__dirname, '../dist');
 const jsonOutput = path.resolve(distDir, 'solution.json');
-const csvOutput = path.resolve(distDir, 'solution.csv');
 
 // console colors: ----------
 const reset = '\x1b[0m';
@@ -22,46 +21,34 @@ const optionsJSON = {
   quote: '"'
 };
 
-const optionsCSV = {
-  headers: 'key',
-  delimiter: ',',
-  wrap: false
+const transformSolution = ({ id, data }) => ({
+  _id: id.replace(/(_)/g, '-'),
+  contentHtml: data.contentHtml || undefined,
+  subtitle: data.subtitle || undefined,
+  title: data.title || undefined,
+  topic: data.topic || undefined,
+  note: data.note || undefined,
+  template: data.template || undefined,
+  readTime: data.readTime || undefined,
+  allowContact: data.allowContact || undefined,
+  actionText: data.action ? data.action.text : undefined,
+  actionUrl: data.action ? data.action.url : undefined
+});
+
+const findSolutionById = solution => id => {
+  const elem = solution.find(item => item.id === id);
+  if (elem === undefined) {
+    throw new Error(`Not Found! id=${id}`);
+  }
+  return elem;
 };
 
-function transformSolution({ id, data }) {
-  return {
-    _id: id.replace(/(_)/g, '-'),
-    contentHtml: data.contentHtml || undefined,
-    subtitle: data.subtitle || undefined,
-    title: data.title || undefined,
-    topic: data.topic || undefined,
-    note: data.note || undefined,
-    template: data.template || undefined,
-    readTime: data.readTime || undefined,
-    allowContact: data.allowContact || undefined,
-    actionText: data.action ? data.action.text : undefined,
-    actionUrl: data.action ? data.action.url : undefined
-  };
-}
+const parseSolution = ({ id, data }) => ({
+  id,
+  data: JSON.parse(unescape(data))
+});
 
-function findSolutionById(solution) {
-  return function(id) {
-    const elem = solution.find(item => item.id === id);
-    if (elem === undefined) {
-      throw new Error(`Not Found! id=${id}`);
-    }
-    return elem;
-  };
-}
-
-function parseSolution({ id, data }) {
-  return {
-    id,
-    data: JSON.parse(unescape(data))
-  };
-}
-
-function main() {
+(() => {
   try {
     if (!fs.existsSync(distDir)) {
       fs.mkdirSync(distDir);
@@ -75,21 +62,16 @@ function main() {
       .map(parseSolution)
       .map(transformSolution);
 
-    const csvResult = csvjson.toCSV(jsonResult, optionsCSV);
-
     fs.writeFileSync(jsonOutput, JSON.stringify(jsonResult));
-    fs.writeFileSync(csvOutput, csvResult);
 
     console.info(
       greenColor,
-      'Yeah! Done! Your files are here:',
+      'Yeah! Done! Your file is here:',
       blueColor,
-      `\n${jsonOutput}\n${csvOutput}`,
+      `\n${jsonOutput}`,
       reset
     );
   } catch (error) {
     console.error(redColor, `Oh, no! ${error.message}`, reset);
   }
-}
-
-main();
+})();
